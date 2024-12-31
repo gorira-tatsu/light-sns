4d24b56eb174b94d132889d03db9ca66c5f80640
gorira <tatsuhiko.shigoto@gmail.com>
2024-12-31 12:30:54 +0900

    全てDatabaseに置換完了

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'hono'
-import { basicAuth } from 'hono/basic-auth'
 import {
   getCookie,
   getSignedCookie,
@@ -8,25 +7,37 @@ import {
   deleteCookie,
 } from 'hono/cookie'
 import { createClient } from '@supabase/supabase-js'
-import { formatISO, nextDay } from "date-fns";
+import { format, formatISO, nextDay } from "date-fns";
 import { HTTPException } from 'hono/http-exception'
 import { loginAuthrizationInfomation, signupAuthrizationInfomation } from './schama/auth.schama';
 import { ZodError } from 'zod';
 import { password } from 'bun';
 import { Prisma, PrismaClient } from '@prisma/client'
+import { use } from 'hono/jsx';
 
 const app = new Hono()
 
 const prisma = new PrismaClient()
 
-let users : {
-  [user_id: string]: {password: string, salt: string}
-} = {
-  "admin": {password: "CT5MXoRhKrEmekATmm5h8vjjbhUwaTH+ugc8gANn6rY", salt: "aP6GF++Z/VbytX6pT3QSynlRyBNwkJ19F0VfXImVOYM"} //password:password
-}
+export const verifyUser = async (session_id: string) => {
 
-let session_ids : {[uuid: string]: string} = {
-  "922d2784-e860-4d5f-b42b-6b03b502cbb5": "admin"
+  const user_idHandler = await prisma.sessionIDs.findUnique({
+    where: {
+      UUID: session_id
+    },
+    select: {
+      userId: true,
+      UUID: true
+    }
+  })
+
+  if (user_idHandler) {
+    return user_idHandler["userId"]
+  } else {
+    throw new HTTPException(401, {
+      message: "Not found user in session_ids"
+    })
+  }
 }
 
 app.post('/login', async (c) => {
@@ -36,6 +47,11 @@ app.post('/login', async (c) => {
         const getAimedUserAuthInfo = await prisma.userAuth.findUnique({
             where: {
                 userId: user_id
+            },
+            select: {
+              userId: true,
+              password: true,
+              salt: true,
             }
         })
 
@@ -44,12 +60,19 @@ app.post('/login', async (c) => {
         const aimedUserHash = `$argon2id$v=19$m=65536,t=2,p=1$${aimedUserSalt}$${aimedUserPassword}`
 
         if (await Bun.password.verify(password, aimedUserHash)) {
-        const uuid = crypto.randomUUID();
+          const uuid = crypto.randomUUID();
 
-        setCookie(c, 'session_id', uuid)
-        session_ids[uuid] = user_id
+          setCookie(c, 'session_id', uuid)
 
-        return c.text("login success", 200)
+          const sessionIdHandler = await prisma.sessionIDs.create({
+            data: {
+              createdAt: formatISO(new Date()),
+              userId: user_id,
+              UUID: uuid
+            }
+          })
+
+          return c.text("login success", 200)
         }
         else {
         throw new HTTPException(401)
@@ -75,8 +98,11 @@ app.post('/signup', async (c) => {
 try {
     const {user_id, user_name, password} = signupAuthrizationInfomation.parse(await c.req.json());
 
-    const alreadyUser = await prisma.user.findUnique({
-        where: { userId: user_id }
+    const alreadyUser = await prisma.userAuth.findUnique({
+        where: { userId: user_id },
+        select: {
+          userId: true
+        }
     })
 
     if (!alreadyUser) {
--- src/index.ts
+++ src/index.ts
@@ -11,44 +11,21 @@ import { createClient } from '@supabase/supabase-js'
 import { formatISO, nextDay } from "date-fns";
 import { HTTPException } from 'hono/http-exception'
 import { loginAuthrizationInfomation, signupAuthrizationInfomation } from './schama/auth.schama';
-import { ZodError } from 'zod';
+import { boolean, date, ZodError } from 'zod';
 import { password } from 'bun';
 import AuthRoute from "./auth";
+import DebugRoute from "./debug"
+import { verifyUser } from "./auth"
 import { PrismaClient } from '@prisma/client'
 
 const app = new Hono()
 
-app.route("/auth",AuthRoute)
+app.route("/auth", AuthRoute)
+app.route("/debug", DebugRoute)
 
 const prisma = new PrismaClient()
 
-let users : {
-  [user_id: string]: {password: string, salt: string}
-} = {
-  "admin": {password: "CT5MXoRhKrEmekATmm5h8vjjbhUwaTH+ugc8gANn6rY", salt: "aP6GF++Z/VbytX6pT3QSynlRyBNwkJ19F0VfXImVOYM"} //password:password
-}
-
-let session_ids : {[uuid: string]: string} = {
-  "922d2784-e860-4d5f-b42b-6b03b502cbb5": "admin"
-}
-
 let posts : Array<{user_id: string, date: string, body: string}> = []
-
-const loginUser = (user_id: string, password: string): boolean => {
-  return password === users[user_id]["password"]
-}
-
-const verifyUser = (session_id: string) => {
-  const user_id = session_ids[session_id]
-
-  if (user_id) {
-    return user_id
-  } else {
-    throw new HTTPException(401, {
-      message: "Not found user in session_ids"
-    })
-  }
-}
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -61,12 +38,16 @@ app.post('/service/post', async (c) => {
   } = await c.req.json()
 
   try {
-    const user_id = verifyUser(session_id)
-    posts.push({
-      user_id : user_id,
-      date: formatISO(new Date()),
-      body: body
+    const user_id = await verifyUser(session_id)
+
+    const postHandler = await prisma.post.create({
+      data: {
+        createdAt: formatISO(new Date()),
+        userId: user_id,
+        body: body,
+      }
     })
+
   } catch (e) {
     if (e instanceof HTTPException) {
       throw e
@@ -80,18 +61,39 @@ app.post('/service/post', async (c) => {
   return c.text("success", 200)
 })
 
+
 app.get('/service/getTimeline', async (c) => {
-  return c.json(posts.slice(0, 200))
+
+  try
+  {  
+  
+  const totalPosts = await prisma.post.count();
+  const takeCount = Math.min(totalPosts, 200);
+  const postGetHandler = await prisma.post.findMany({
+    orderBy: {
+      id: "desc"
+    },
+    take: takeCount
+  })
+
+  const forJsonPosts = postGetHandler.map((post) => { // JSON can't handle BIGINT
+    return {
+      id: String(post.id),
+      createdAt: post.createdAt,
+      userId: post.userId,
+      body: post.body
+    }
+  })
+
+  return c.json(forJsonPosts)
+
+  }
+  catch(e) {
+    console.log(e)
+  }
+
 })
 
-
-app.get('/system/show_user', async (c) => {
-  return c.json(users)
-})
-
-app.get('/system/show_session_ids', async(c) => {
-  return c.json(session_ids)
-})
 
 app.onError((error, c) => {
   if  (error instanceof HTTPException) {
